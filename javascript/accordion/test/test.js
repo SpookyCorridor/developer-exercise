a510cbdb509bab9726ea8f2c7ea35b508738dc1a
Dylan <invision33@gmail.com>
2015-10-24 02:14:16 -0500

    add failing test should hide all other children

--- javascript/accordion/test/test.js
+++ javascript/accordion/test/test.js
@@ -11,4 +11,12 @@ describe('Accordion', function() {
 		var firstChildStyle = $('.accordion-header').eq(0).children(':first-child').css('display');
 		expect(firstChildStyle).to.not.equal('none');
 	});
+
+	it('should hide all other children of accordion-header', function() {
+		var otherChildren = $('.accordion-header').eq(0).children().not(':first-child');
+		$.each(otherChildren, function(child) {
+			expect(otherChildren[child].style.display).to.equal('none'); 
+		})
+	})
+
 })
