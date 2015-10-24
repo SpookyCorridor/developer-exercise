2f1228c19a692f57b8cb8b199a242266b463995f
Dylan <invision33@gmail.com>
2015-10-24 03:07:10 -0500

    enclosing and cleaning up app.js

--- javascript/accordion/app.js
+++ javascript/accordion/app.js
@@ -1,13 +1,17 @@
-function accordion() {
-	var wrappers = $('.accordion-wrapper'); 
-	var children = $('.accordion-wrapper').children();
+$(function() {
+	function accordion() {
+		var wrappers = $('.accordion-wrapper'); 
+		var children = $('.accordion-wrapper').children();
 
-	$.each(wrappers, function(idx, wrapper) {
-		$(this).find('> * > *').not(':first-child').hide(); 
-	});
+		$.each(wrappers, function(idx, wrapper) {
+			$(this).find('> * > *').not(':first-child').hide(); 
+		});
 
-	$($(children)).on('click', function(){
-	  $(this).children().not(':first-child').slideToggle('slow');
-	  $(this).siblings().children().not(':first-child').slideUp("slow"); 
-	})
-}
+		$($(children)).on('click', function(){
+		  $(this).children().not(':first-child').slideToggle('slow');
+		  $(this).siblings().children().not(':first-child').slideUp("slow"); 
+		});
+	}
+
+	accordion();
+});
