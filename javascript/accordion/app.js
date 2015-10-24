da351539ca7238c82bf71d7bbba2bdcf6193c5ff
Dylan <invision33@gmail.com>
2015-10-24 02:32:52 -0500

    hide all other children test passes

--- javascript/accordion/app.js
+++ javascript/accordion/app.js
@@ -1,3 +1,9 @@
 function accordion() {
-	
+	var wrappers = $('.accordion-wrapper'); 
+	var children = $('.accordion-wrapper').children();
+
+	$.each(wrappers, function(idx, wrapper) {
+		$(this).find('> * > *').not(':first-child').hide(); 
+	});
+
 }
