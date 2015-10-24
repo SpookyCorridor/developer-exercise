21ab9ca4db11a5b7a596c1ce0cd66eaaadc81db7
Dylan <invision33@gmail.com>
2015-10-24 02:09:04 -0500

    fix test jquery and add first passing accordion test

--- javascript/accordion/test/test.js
+++ javascript/accordion/test/test.js
@@ -3,4 +3,12 @@ describe('load test', function() {
 		var pass = true; 
 		expect(pass).to.be.true;
 	})
+});
+
+describe('Accordion', function() {
+	it('should display first child of accordion-header', function() {
+		accordion(); 
+		var firstChildStyle = $('.accordion-header').eq(0).children(':first-child').css('display');
+		expect(firstChildStyle).to.not.equal('none');
+	});
 })
