42bbea82d11b107c423df539332d1644728d9e10
Dylan <invision33@gmail.com>
2015-10-25 16:52:35 -0500

    fix test calls and move packages out to root

--- javascript/accordion/test/test.js
+++ javascript/accordion/test/test.js
@@ -7,7 +7,6 @@ describe('load test', function() {
 
 describe('Accordion', function() {
 	it('should display first child of accordion-header', function() {
-		accordion(); 
 		var firstChildStyle = $('.accordion-header').eq(0).children(':first-child').css('display');
 		expect(firstChildStyle).to.not.equal('none');
 	});
