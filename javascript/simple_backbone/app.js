be0249f0868697338ec59bf71a4cc7d90ad9418a
Dylan <invision33@gmail.com>
2015-10-26 01:11:33 -0500

    start adding ajax and rendering functions

--- javascript/simple_backbone/app.js
+++ javascript/simple_backbone/app.js
@@ -8,15 +8,57 @@ var QuoteList = Backbone.Collection.extend({
 var QuoteView = Backbone.View.extend({
 	el: "#quotes",
 	template: _.template($('#quotes-template').html()),
+	page: 0,
+	perPage: 15,
+	totalPages: 0,
+	loadMore: null,
 
 	initialize: function() {
 		var that = this; 
+		this.loadMore = new loadMoreQuotesView();
 		this.collection = new QuoteList; 
+		this.collection.on('reset', function() {
+			that.render();
+		}, this)
 		this.render(); 
 	},
 
 	render: function() {
 		var that = this; 
-		return this; 
-	} 
+		this.collection.fetch({
+			success: function(data) {
+				that.totalPages = Math.ceil(_.size(that.collection.models) / that.perPage);
+					return that.renderQuoteGroup(0, that.perPage - 1);
+			}
+		});
+	},
+
+	renderQuoteGroup: function(start, end) {
+			// console.log('rendering...');
+			var subset = _.filter(this.collection.models, function(num, index){
+				return (index >= start) && (index <= end);
+			});
+			
+			_.each(subset, function(quote) {
+				var quoteTemplate = this.template(quote.toJSON());
+				$(this.el).append(quoteTemplate);
+			}, this);
+
+			this.renderLoadMoreButton();
+			return this;
+		}, 
+
+		renderLoadMoreButton: function() {
+			if(this.page >= (this.totalPages - 1)) {
+				this.loadMore.$el.hide();
+			} else {
+				this.$el.append(this.loadMore.$el.detach().show());
+				}
+			}
+	});
+
+	var loadMoreQuotesView = Backbone.View.extend({
+		el: $("#load-more-quotes")
+	});
+
 });
