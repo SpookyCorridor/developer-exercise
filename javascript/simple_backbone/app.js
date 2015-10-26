b8ad523a39c5a941df7e280b1dda16353613be7d
Dylan <invision33@gmail.com>
2015-10-26 00:38:36 -0500

    minor test fixes and add initial view

--- javascript/simple_backbone/app.js
+++ javascript/simple_backbone/app.js
@@ -4,3 +4,19 @@ var QuoteList = Backbone.Collection.extend({
 	model: Quote,
 	url: 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json'
 });
+
+var QuoteView = Backbone.View.extend({
+	el: "#quotes",
+	template: _.template($('#quotes-template').html()),
+
+	initialize: function() {
+		var that = this; 
+		this.collection = new QuoteList; 
+		this.render(); 
+	},
+
+	render: function() {
+		var that = this; 
+		return this; 
+	} 
+});
--- javascript/simple_backbone/test/test.js
+++ javascript/simple_backbone/test/test.js
@@ -1,17 +1,16 @@
 describe("initialize testing", function() {
 
-	var apiLink = 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json'
+	var apiLink = 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json';
 
 	describe("collection", function() {
 		beforeEach(function() {
-			this.model = new Quote; 
+			this.model = new Quote(); 
 			this.collection = new QuoteList(); 
-		})
+		});
 
 		it("should have correct default 'url' property", function(){		 
 			this.collection.url.should.equal(apiLink);
 		});
-		
-	});
 
+	});
 });
