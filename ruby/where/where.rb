6c0bc2c0c8d170a96cc2d18e65637e09dc958882
Dylan <invision33@gmail.com>
2015-10-27 17:30:30 -0500

    all tests now passing for module yay

--- ruby/where/where.rb
+++ ruby/where/where.rb
@@ -1,28 +1,25 @@
-module Where 
-
-    def where(args)
-    	@keys = []
-    	@vals = []
-    	count = 0
-    	@matches = [] 
-        args.each do |key, value|
-        	@keys.push(key)
-        	@vals.push(value)
-        end
-        puts @keys
-        puts @vals
-        self.each do |obj|
-        	@keys.length.times do |i|
-        		if obj[@keys[i]].to_s =~ Regexp.new(@vals[i].to_s)
-        			@matches.push(obj)
-        		else 
-        			puts 'no'
-        		end 
-        	end 
-        	# next unless obj[@keys[count]].include?(@vals[count])
-        	# puts 'what'
-        	# puts obj[@keys[1]]
-        end 
-         @matches
+module Where
+  def where(args)
+  	@keys = []
+  	@vals = []
+  	@matches = [] 
+    args.each do |key, value|
+    	@keys.push(key)
+    	@vals.push(value)
     end
-end
+    self.each do |obj|
+    	@full_match = 0 
+    	@keys.length.times do |i|
+    		 @vals[i] = @vals[i].to_s.tr('/', '')
+    		 
+    		if obj[@keys[i]].to_s =~ Regexp.new(@vals[i].to_s)
+    			@full_match += 1   
+    		end 
+    	end
+    	if @full_match == @keys.length 
+    			@matches.push(obj)
+    	end  
+    end 
+    @matches
+  end
+end 
